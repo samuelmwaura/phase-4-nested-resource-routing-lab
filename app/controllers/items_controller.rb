fd9037a0e26263ff5fea6584cd7732e500049f46
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-10-05 15:34:38 +0300

    Completed the lab

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,39 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_item_not_found
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id] #Checks if the user for which the items should be found has been specified
+      user = User.find(params[:user_id])
+      items = user.items
+    else
+      items = Item.all
+    end
+      render json: items, include: :user
+  end
+
+
+  def show  #This will work even for nested routes since the Id value is the same
+    render json: Item.find(params[:id]), include: :user
+  end
+
+  def create
+   if params[:user_id]
+      user = User.find(params[:user_id])
+      item = user.items.create(item_params)
+      #user.items << Item.create(item_params) - This is another way to create an item related to a given user but you wont be able to assign the create item to a variable inorder to render
+   else
+     item = Item.create(item_params)
+   end
+   render json: item , status: :created
+  end
+
+  private
+  def render_item_not_found
+    render json: {error: "Record not found!"}, status: :not_found
+  end
+
+  def item_params
+    params.permit(:name,:description, :price)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
-  resources :users, only: [:show]
+  resources :users, only: [:show] do
+    resources :items ,only: [:index,:show] #Means that within an individual user, we have a way to access all the items that are related to them and a singular item too.
+    #It will produce the routes "users/:user_id/items" and "users/:user_id/items/:item_id"
+  end
+
+
+  resources :users,only: [:create] do #This is nesting for the item creation functionality
+    resources :items, only: [:create]
+ end
 end
